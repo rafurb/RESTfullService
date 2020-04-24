798d2852084d8915c91ee8ffdf0e4ce56fce9176
rafalurbas <rafalurbas@192.168.8.104>
2020-04-24 12:11:06 +0200

    First

---
+++ RESTfulWebService/src/main/java/com/rafurb/RESTfulWebService/Greeting.java
@@ -0,0 +1,22 @@
+package com.rafurb.RESTfulWebService;
+
+public class Greeting {
+	private long id;
+	private String content;
+	
+	public Greeting(long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	
+}
---
+++ RESTfulWebService/src/main/java/com/rafurb/RESTfulWebService/ResTfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rafurb.RESTfulWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulWebServiceApplication.class, args);
+	}
+
+}
